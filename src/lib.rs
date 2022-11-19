efb43bfeb1448bce7b6025c49fe30f1d81cf2e45
al8n <scygliu1@gmail.com>
2022-11-20 01:55:30 +0800

    update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 //! Porting Go standard library in Rust
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/al8n/crabmole/main/art/crabmole.jpg"
+)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
 
