65bcb997737a0c38307c4ba1002810ac0f1c4d0b
al8n <scygliu1@gmail.com>
2022-11-20 01:31:17 +0800

    finish sort

---
+++ README.md
@@ -0,0 +1,10 @@
+# crabmole
+Porting some Go standard libraries in Rust
+
+**Note:** This crate will not port all go standard libraries in Rust, but some missing libraries in Rust environment.
+
+
+## Status
+| name | status |
+|------|--------|
+| sort |  beta  |
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! Porting Go standard library in Rust
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, allow(unused_attributes))]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+/// Go sort library
+#[cfg(feature = "sort")]
+#[cfg_attr(docsrs, doc(cfg(feature = "sort")))]
+pub mod sort;
